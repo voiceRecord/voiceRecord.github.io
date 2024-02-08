7fb38354e1aac5b841d63c40def8d16e75a9d9b4
te-lyhourt <74485952+te-lyhourt@users.noreply.github.com>
2024-02-08 18:59:00 +0100

    fix current bug add replacing function

--- src/views/audio.vue
+++ src/views/audio.vue
@@ -22,7 +22,7 @@
           Stop
         </button>
       </div>
-      <h2 style="margin-top: 20px;">Recorded {{ currentText-1 }} words over {{ content.length }}</h2>
+      <h2 style="margin-top: 20px;">Recorded {{ currentText }} words over {{ content.length }}</h2>
       <h2 style="margin-top: 20px;">Recordings </h2>
       <ol id="recordingsList"></ol>
       <li v-for="audio in audioList" :key="audio">
@@ -41,7 +41,7 @@
 import router from "@/router";
 import {onMounted,reactive,ref} from "vue"
 import axios from 'axios';
-import {ref as storageRef, uploadBytes ,list,getDownloadURL } from 'firebase/storage'
+import {ref as storageRef, uploadBytes ,list,getDownloadURL,deleteObject} from 'firebase/storage'
 import {storage,db} from '@/firebase/firebase'
 import { doc, updateDoc } from "firebase/firestore";
 
@@ -121,7 +121,7 @@ const listMorePage = async (saveAudios) =>{
 const saveRecord = (audio) =>{
   // save new or replace
   if(audio.audioTextNumber==currentText){
-    let text = "Do you want to save the record to database!\n Once save you cannot delete it\nEnter OK to confirm";
+    let text = "Do you want to save the record to database!\nEnter OK to confirm";
     if (confirm(text) == true) {
       audio.disabled = true;
       audio.saveState = 'uploading...'
@@ -164,38 +164,23 @@ const saveRecord = (audio) =>{
       });
       
       //get the uploaded audio and delete it from both local and server
-      audioList.forEach((audioInList)=>{
-        let audioName = audioInList.audioName.toString()
-        let text = audio.text.toString()
-
-        console.log(audioName+"\ " +text);
-        console.log(audioName.includes(text))
-        // if(audioInList.audioName.includes(audio.text)&&saveState=="uploaded !"){
-        //   deleteObject(audioInList.ref).then(()=>{
-        //     const index = audioList.indexOf(audioInList);
-        //     if (index !== -1) {
-        //       audioList.splice(index, 1);
-        //     }
-        //   })
-        // }
-      })
+      for(let i = 0; i<audioList.length;i++){
+        let item = audioList[i]
+        if(item.audioName.includes(audio.text.trim())&& item.saveState=="uploaded !"){
+          deleteObject(item.ref).then(()=>{
+            const index = audioList.indexOf(item);
+            if (index !== -1) {
+              audioList.splice(index, 1);
+            }
+          })
+          break
+        }
+      }
     }
   }
   
 }
-// const deleteRecord = (audio) =>{
-//   let text = "Do you want to delete the record from database!\nEnter OK to confirm";
-//   if (confirm(text) == true) {
-//     audio.deleteState = 'deleting...'
-//     deleteObject(audio.ref).then(()=>{
-//       audio.deleteState = 'deleted'
-//       const index = audioList.indexOf(audio);
-//       if (index !== -1) {
-//         audioList.splice(index, 1);
-//       }
-//     })
-//   }
-// }
+
 
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
