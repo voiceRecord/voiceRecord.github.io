924815286ad22449dfad70475997f31510b259cf
te-lyhourt <74485952+te-lyhourt@users.noreply.github.com>
2024-02-12 10:48:12 +0100

    add 404

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    // Configure Vite to serve index.html for all routes
-    fs: {
-      strict: false
-    }
-  }
 })
